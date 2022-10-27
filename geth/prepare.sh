a0f3601509f9c8adc6b7e5006f9b8d5d8eca74f6
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2022-10-27 17:51:17 +0800

    Update prepare.sh

--- geth/prepare.sh
+++ geth/prepare.sh
@@ -1,4 +1,6 @@
 # download some
-curl https://gethstore.blob.core.windows.net/builds/geth-darwin-amd64-1.10.21-unstable-ae8ce720.tar.gz
-tar -zxvf geth-darwin-amd64-1.10.21-unstable-ae8ce720.tar.gz
-cp geth-darwin-amd64-1.10.20-8f2416a8/geth /usr/local/bin/geth
+git clone -b v1.10.25 https://github.com/ethereum/go-ethereum.git
+# tar -zxvf geth-darwin-amd64-1.10.21-unstable-ae8ce720.tar.gz
+cd go-ethereum
+make geth
+sudo cp build/bin/geth /usr/local/bin/geth
