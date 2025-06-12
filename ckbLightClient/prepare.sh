2d393eaadc082a17a70108a925960f923961388e
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2025-06-12 21:04:15 +0800

    Update prepare.sh

--- ckbLightClient/prepare.sh
+++ ckbLightClient/prepare.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/nervosnetwork/ckb-light-client.git
 cd ckb-light-client
-git checkout develop
+git checkout update-fee-rate-checker
 cargo build --release
 cp ../config.toml target/release/
