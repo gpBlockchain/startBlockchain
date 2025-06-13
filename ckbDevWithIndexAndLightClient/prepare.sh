a16704b36a10df327897020f96667b807d0676b2
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2025-06-13 09:43:48 +0800

    Update prepare.sh

--- ckbDevWithIndexAndLightClient/prepare.sh
+++ ckbDevWithIndexAndLightClient/prepare.sh
@@ -2,11 +2,8 @@
 # clone ckbIndex
 # clone ckb -light client
 git clone https://github.com/nervosnetwork/ckb.git
-git clone https://github.com/nervosnetwork/ckb-indexer.git
 git clone https://github.com/nervosnetwork/ckb-light-client.git
 cd ckb
 make build
-cd ../ckb-indexer
-cargo build --release
 cd ../ckb-light-client
 cargo build --release
